0066119968017f7133e196d231a824c7ab15fb32
Daniel LIma <danie_ner@hotmail.com>
2019-07-03 16:00:22 -0300

    a2

--- js/master.js
+++ js/master.js
@@ -1,15 +1,17 @@
 server = "http://localhost/pessoal/CubeBlack.github.io.git/";
 eleNav = document.getElementById('menu');
+eleContent = document.getElementById("conteudo");
 menuArr = null;
 //---------------
 wfile = new wrequest();
-function open(url) {
-  send  = server+url;
+converter = new showdown.Converter();
+function view(url) {
+  if (url[0] == "h") send = url;
+  else  send  = server+url;
   wfile.postMessage(send);
   wfile.onmessage = function (event) {
-    converter = new showdown.Converter();
-    //html = converter.makeHtml(text);
-    //console.log(html);
+    console.log(event.data);
+    eleContent.innerHTML = converter.makeHtml(event.data);
   }
 }
 //------------
@@ -24,11 +26,20 @@ function loadMenu(id){
   wmenu.postMessage(send);
   wmenu.onmessage = function (event) {
     menuArr = JSON.parse(event.data);
-    console.log(menuArr);
-    eleNav.innerHTML = event.data;
+    menu = document.createElement("ul");
+    for (var i = 0; i < menuArr.length; i++) {
+      //menuHTML += menuArr[i].label;
+       link = document.createElement("li");
+       inner = document.createTextNode(menuArr[i].label);
+       link.setAttribute('onclick',"view('"+menuArr[i].label+"')");
+       link.appendChild(inner);
+       menu.appendChild(link);
+    }
+    //console.log(menuArr);
+    eleNav.appendChild(menu);
   }
 }
 //caregar o menu
 loadMenu();
-open("README.md");
+view("README.md");
 console.log("master.js");
